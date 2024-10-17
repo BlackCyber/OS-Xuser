1df5dd41c8a92dbddbfbb2e64f8505d7060b402c
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:08:27 +0700

    gui.pas
    Graphical User Interface (GUI):
    
    Implementasi GUI memerlukan manajemen jendela, input mouse, dan rendering grafis.

---
+++ gui.pas
@@ -0,0 +1,16 @@
+{ gui.pas }
+
+procedure DrawWindow(X, Y, Width, Height: Integer);
+begin
+  { Draw a window with borders }
+end;
+
+procedure HandleMouseInput;
+begin
+  { Handle mouse input events }
+end;
+
+procedure GUI;
+begin
+  { Initialize and manage GUI components }
+end;
