a1279bf2bc5744dd20761d9371a5af4dbc8c70de
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:26:23 +0700

    app_framework.pas
    Menyediakan Dukungan Aplikasi:
    
    Tawarkan kerangka kerja untuk pengembangan aplikasi, termasuk pustaka untuk antarmuka grafis dan utilitas.

---
+++ app_framework.pas
@@ -0,0 +1,11 @@
+{ app_framework.pas }
+
+procedure RunApplication(AppName: String);
+begin
+  { Load and run the specified application }
+end;
+
+procedure RegisterApplication(AppName: String);
+begin
+  { Register a new application with the OS }
+end;
