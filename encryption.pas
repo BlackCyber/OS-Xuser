49b7308f26ee4364c968015ef730443c223fc149
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:23:25 +0700

    encryption.pas
    Enkripsi dan Dekripsi:
    
    Implementasikan algoritma enkripsi sederhana untuk melindungi data.

---
+++ encryption.pas
@@ -0,0 +1,11 @@
+{ encryption.pas }
+
+function EncryptData(Data: String; Key: String): String;
+begin
+  { Simple encryption algorithm }
+end;
+
+function DecryptData(Data: String; Key: String): String;
+begin
+  { Simple decryption algorithm }
+end;
