f2395558c6b131971a5b30089d758f76838abfe6
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:25:28 +0700

    system_api.pas
    Pengembangan Aplikasi dan API Sistem
    Untuk memperluas fungsionalitas OS, Anda dapat mengembangkan API sistem yang memungkinkan aplikasi berinteraksi dengan OS.
    
     Membuat API Sistem:
    
    Definisikan API untuk operasi dasar seperti file I/O, manajemen memori, dan proses.

---
+++ system_api.pas
@@ -0,0 +1,21 @@
+{ system_api.pas }
+
+procedure CreateFile(FileName: String);
+begin
+  { Create a new file }
+end;
+
+procedure DeleteFile(FileName: String);
+begin
+  { Delete a file }
+end;
+
+procedure WriteToFile(FileName: String; Data: String);
+begin
+  { Write data to a file }
+end;
+
+procedure ReadFromFile(FileName: String; var Data: String);
+begin
+  { Read data from a file }
+end;
