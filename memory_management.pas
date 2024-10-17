cc7245f40ad310b4eda5b307bcca0ab6812c6096
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:29:46 +0700

    Create memory_management.pas

---
+++ memory_management.pas
@@ -0,0 +1,11 @@
+{ memory_management.pas }
+
+procedure CompactMemory;
+begin
+  { Compact fragmented memory }
+end;
+
+procedure DefragmentMemory;
+begin
+  { Defragment memory for improved performance }
+end;
