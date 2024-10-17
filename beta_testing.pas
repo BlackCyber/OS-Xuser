f7bcf4ac09f6f14f1d4be9e7d49c255871147673
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:36:57 +0700

    beta_testing.pas
    Pengujian Beta:
    
    Lakukan pengujian beta dengan sekelompok pengguna untuk mendapatkan umpan balik sebelum rilis penuh.

---
+++ beta_testing.pas
@@ -0,0 +1,11 @@
+{ beta_testing.pas }
+
+procedure RecruitBetaTesters;
+begin
+  { Recruit users for beta testing }
+end;
+
+procedure CollectBetaFeedback;
+begin
+  { Collect and analyze feedback from beta testers }
+end;
