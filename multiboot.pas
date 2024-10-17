20c17c97195e329f706520a9bbb05d449c103612
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:29:16 +0700

    multiboot.pas
    Dukungan Multi-Boot:
    
    Tambahkan kemampuan untuk memilih dan mem-boot berbagai OS dari satu media.

---
+++ multiboot.pas
@@ -0,0 +1,11 @@
+{ multiboot.pas }
+
+procedure DisplayBootMenu;
+begin
+  { Display a menu for selecting the OS to boot }
+end;
+
+procedure LoadOS(Selection: Integer);
+begin
+  { Load the selected OS }
+end;
