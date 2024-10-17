fd275c80a7ee2df9bbea2179b512edc134bcd2c0
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:06:32 +0700

    vga.pas
    Menampilkan Grafis VGA Video Grafis Application

---
+++ vga.pas
@@ -0,0 +1,11 @@
+{ vga.pas }
+
+procedure SetPixel(X, Y: Integer; Color: Byte);
+begin
+  { Set pixel color in VGA mode }
+end;
+
+procedure DrawLine(X1, Y1, X2, Y2: Integer; Color: Byte);
+begin
+  { Draw a line between two points }
+end;
