d1346ca1680e881a994f3a642bffc9a5741a7f01
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:14:34 +0700

    input.pas
    Pengelolaan Input Pengguna:

---
+++ input.pas
@@ -0,0 +1,11 @@
+{ input.pas }
+
+procedure HandleKeyboardInput;
+begin
+  { Handle keyboard input and pass it to GUI components }
+end;
+
+procedure HandleMouseInput;
+begin
+  { Handle mouse input and pass it to GUI components }
+end;
