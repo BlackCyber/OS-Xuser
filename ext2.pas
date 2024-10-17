674663363bec4609c66c5f9c6f7e016e017ab46a
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:20:58 +0700

    ext2.pas
    Dukungan untuk Sistem File yang Lebih Kompleks:
    
    Tambahkan dukungan untuk sistem file yang lebih kompleks, seperti ext2 atau FAT16, dengan struktur direktori yang lebih canggih dan alokasi file.

---
+++ ext2.pas
@@ -0,0 +1,16 @@
+{ ext2.pas }
+
+procedure InitializeExt2FileSystem;
+begin
+  { Initialize ext2 file system structures }
+end;
+
+procedure ReadExt2File(FileName: String; Buffer: Pointer);
+begin
+  { Read file from ext2 file system }
+end;
+
+procedure WriteExt2File(FileName: String; Buffer: Pointer);
+begin
+  { Write file to ext2 file system }
+end;
