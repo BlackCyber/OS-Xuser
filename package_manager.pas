4edcd12190ac35bbac3d55054f42bea09ba4c36d
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:19:27 +0700

    package_manager.pas
    Fitur Tambahan
    1. Dukungan untuk Manajemen Paket:
    
    Implementasikan sistem manajemen paket sederhana untuk mengelola aplikasi dan pustaka di OS.

---
+++ package_manager.pas
@@ -0,0 +1,11 @@
+{ package_manager.pas }
+
+procedure InstallPackage(PackageName: String);
+begin
+  { Install package from disk or network }
+end;
+
+procedure RemovePackage(PackageName: String);
+begin
+  { Remove installed package }
+end;
