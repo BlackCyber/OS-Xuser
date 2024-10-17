97d83bfbb72c25f19fd9c4b486012e062e857d7d
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:24:03 +0700

    power_management.pas
    Pengelolaan Energi dan Manajemen Daya
    Manajemen daya sangat penting untuk sistem yang berjalan di perangkat portabel seperti laptop.
    
    1. Mode Tidur dan Hibernasi:
    
    Tambahkan dukungan untuk mode tidur dan hibernasi.

---
+++ power_management.pas
@@ -0,0 +1,21 @@
+{ power_management.pas }
+
+procedure EnterSleepMode;
+begin
+  { Save system state and enter sleep mode }
+end;
+
+procedure WakeUpFromSleep;
+begin
+  { Restore system state and wake up }
+end;
+
+procedure EnterHibernateMode;
+begin
+  { Save system state to disk and enter hibernation }
+end;
+
+procedure WakeUpFromHibernate;
+begin
+  { Restore system state from disk }
+end;
