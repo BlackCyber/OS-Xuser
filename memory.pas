0b62e857fcd9a5fddf39c0e8895b4fcd72e5a677
Kus Hendar <kushendarmaulana78@gmail.com>
2024-10-17 22:02:06 +0700

    Create memory.pas
    Manajemen memori yang lebih canggih diperlukan untuk mengelola penggunaan RAM dengan lebih efisien dan mendukung multitasking.
    
    Implementasi Paging:
    
    Paging memungkinkan OS untuk mengelola memori virtual dan fisik dengan lebih baik.

---
+++ memory.pas
@@ -0,0 +1,16 @@
+{ memory.pas }
+
+procedure InitializePaging;
+begin
+  { Setup page tables and enable paging here }
+end;
+
+procedure AllocatePage(Address: Pointer);
+begin
+  { Allocate a single page of memory }
+end;
+
+procedure FreePage(Address: Pointer);
+begin
+  { Free a single page of memory }
+end;
